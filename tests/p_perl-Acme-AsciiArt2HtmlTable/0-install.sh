e1434697a1fc50e35bce482f96cdc470c0a0c43b
Dries Verachtert <dries.verachtert@dries.eu>
2015-09-28 20:43:18 +0200

    add install test for perl-Acme-AsciiArt2HtmlTable

---
+++ tests/p_perl-Acme-AsciiArt2HtmlTable/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Acme-AsciiArt2HtmlTable"
+t_installPackage perl-Acme-AsciiArt2HtmlTable
