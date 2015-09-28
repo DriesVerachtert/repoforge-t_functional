c1e638fca0d9a597bbb642c8b1cfed3596026af0
Dries Verachtert <dries.verachtert@dries.eu>
2015-09-28 20:43:18 +0200

    add install test for perl-Acme-BeyondPerl-ToSQL

---
+++ tests/p_perl-Acme-BeyondPerl-ToSQL/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Acme-BeyondPerl-ToSQL"
+t_installPackage perl-Acme-BeyondPerl-ToSQL
