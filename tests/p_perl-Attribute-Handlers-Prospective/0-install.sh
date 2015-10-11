496bae5605588d94561b668dc38b4a2da5625bb3
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-11 22:06:23 +0200

    add install test for perl-Attribute-Handlers-Prospective

---
+++ tests/p_perl-Attribute-Handlers-Prospective/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Attribute-Handlers-Prospective"
+t_installPackage perl-Attribute-Handlers-Prospective
