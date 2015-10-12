7b047e9c1b26c67fe4f35e83858f6caadd40b165
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-12 19:53:10 +0200

    add install test for perl-Attribute-Persistent

---
+++ tests/p_perl-Attribute-Persistent/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Attribute-Persistent"
+t_installPackage perl-Attribute-Persistent
