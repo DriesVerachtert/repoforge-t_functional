e6faa32002883cd190f11df248f70cafc4153e49
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-21 19:20:05 +0200

    add install test for perl-Catalyst-Component-InstancePerContext

---
+++ tests/p_perl-Catalyst-Component-InstancePerContext/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Catalyst-Component-InstancePerContext"
+t_installPackage perl-Catalyst-Component-InstancePerContext
