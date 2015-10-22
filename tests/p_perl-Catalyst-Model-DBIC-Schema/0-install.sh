d6cc9f535eb18db2c285ff3a8928dd5319bb6b67
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-22 21:37:48 +0200

    add install test for perl-Catalyst-Model-DBIC-Schema

---
+++ tests/p_perl-Catalyst-Model-DBIC-Schema/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Catalyst-Model-DBIC-Schema"
+t_installPackage perl-Catalyst-Model-DBIC-Schema
