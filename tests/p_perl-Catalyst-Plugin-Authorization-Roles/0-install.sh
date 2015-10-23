6bcd83c5b9e8303d361e6c9b99f11d1f387dcc12
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-23 22:31:41 +0200

    add install test for perl-Catalyst-Plugin-Authorization-Roles

---
+++ tests/p_perl-Catalyst-Plugin-Authorization-Roles/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Catalyst-Plugin-Authorization-Roles"
+t_installPackage perl-Catalyst-Plugin-Authorization-Roles
