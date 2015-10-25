f6c5113a7c6fd364b539b61c1cd24f4df38d5e44
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-25 19:44:45 +0100

    add install test for perl-CatalystX-Component-Traits

---
+++ tests/p_perl-CatalystX-Component-Traits/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-CatalystX-Component-Traits"
+t_installPackage perl-CatalystX-Component-Traits
