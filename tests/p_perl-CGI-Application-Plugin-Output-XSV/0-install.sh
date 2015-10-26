bff3cc2da6bc25a3bf6254f5fe998dfc09bfdc0a
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-26 20:44:12 +0100

    add install test for perl-CGI-Application-Plugin-Output-XSV

---
+++ tests/p_perl-CGI-Application-Plugin-Output-XSV/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-CGI-Application-Plugin-Output-XSV"
+t_installPackage perl-CGI-Application-Plugin-Output-XSV
