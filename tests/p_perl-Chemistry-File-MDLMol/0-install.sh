9c9a3b5b4d7ba28c6605def29579ff058d1554c9
Dries Verachtert <dries.verachtert@dries.eu>
2015-10-31 20:04:09 +0100

    add install test for perl-Chemistry-File-MDLMol

---
+++ tests/p_perl-Chemistry-File-MDLMol/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Chemistry-File-MDLMol"
+t_installPackage perl-Chemistry-File-MDLMol
