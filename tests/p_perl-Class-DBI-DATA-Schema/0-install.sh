15ad7ed2ba5f11a9e2dc55e02e770a5c6f178623
Dries Verachtert <dries.verachtert@dries.eu>
2015-11-04 18:45:43 +0100

    add install test for perl-Class-DBI-DATA-Schema

---
+++ tests/p_perl-Class-DBI-DATA-Schema/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Class-DBI-DATA-Schema"
+t_installPackage perl-Class-DBI-DATA-Schema
