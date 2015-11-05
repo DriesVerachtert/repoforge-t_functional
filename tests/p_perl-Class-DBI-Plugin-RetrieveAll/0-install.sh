02bac35663c7a1986d035f500f4bd66112a98c4c
Dries Verachtert <dries.verachtert@dries.eu>
2015-11-05 20:58:04 +0100

    add install test for perl-Class-DBI-Plugin-RetrieveAll

---
+++ tests/p_perl-Class-DBI-Plugin-RetrieveAll/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Class-DBI-Plugin-RetrieveAll"
+t_installPackage perl-Class-DBI-Plugin-RetrieveAll
