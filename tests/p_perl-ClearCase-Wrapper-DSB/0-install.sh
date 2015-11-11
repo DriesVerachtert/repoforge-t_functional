15c5007203178a30137f1661f19adce2b2b5aeaf
Dries Verachtert <dries.verachtert@dries.eu>
2015-11-11 19:36:38 +0100

    add install test for perl-ClearCase-Wrapper-DSB

---
+++ tests/p_perl-ClearCase-Wrapper-DSB/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-ClearCase-Wrapper-DSB"
+t_installPackage perl-ClearCase-Wrapper-DSB
