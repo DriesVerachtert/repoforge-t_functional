ae32afb73ee857c338c7c6c4d967b3633f15d028
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-02 18:51:15 +0100

    add install test for perl-Data-JavaScript-Anon

---
+++ tests/p_perl-Data-JavaScript-Anon/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Data-JavaScript-Anon"
+t_installPackage perl-Data-JavaScript-Anon
