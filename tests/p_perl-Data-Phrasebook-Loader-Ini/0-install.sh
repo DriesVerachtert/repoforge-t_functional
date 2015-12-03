c6e2126c6af59b7d9555f26691fee24a4ccb7b26
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-03 19:42:30 +0100

    add install test for perl-Data-Phrasebook-Loader-Ini

---
+++ tests/p_perl-Data-Phrasebook-Loader-Ini/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Data-Phrasebook-Loader-Ini"
+t_installPackage perl-Data-Phrasebook-Loader-Ini
