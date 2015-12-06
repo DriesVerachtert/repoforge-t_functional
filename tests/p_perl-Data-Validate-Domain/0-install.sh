dbb89b0e9ee97d46242e49c13c38a04fe8d12912
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-06 21:22:07 +0100

    add install test for perl-Data-Validate-Domain

---
+++ tests/p_perl-Data-Validate-Domain/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Data-Validate-Domain"
+t_installPackage perl-Data-Validate-Domain
