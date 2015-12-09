af33a96c38d3889480cc3c97b33c5372dd44aeb5
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-09 18:19:16 +0100

    add install test for perl-DateTime-Calendar-Christian

---
+++ tests/p_perl-DateTime-Calendar-Christian/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-DateTime-Calendar-Christian"
+t_installPackage perl-DateTime-Calendar-Christian
