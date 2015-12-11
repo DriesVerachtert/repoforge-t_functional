c89a5d1b87b3652223c36e2a3f34a39294aa9cbc
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-11 20:22:25 +0100

    add install test for perl-DateTime-Format-HTTP

---
+++ tests/p_perl-DateTime-Format-HTTP/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-DateTime-Format-HTTP"
+t_installPackage perl-DateTime-Format-HTTP
