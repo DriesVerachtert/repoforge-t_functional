8958bb2ffe64b52667c0cf73dbb87643da7ed74e
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-11 20:22:25 +0100

    add install test for perl-DateTime-Format-DateParse

---
+++ tests/p_perl-DateTime-Format-DateParse/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-DateTime-Format-DateParse"
+t_installPackage perl-DateTime-Format-DateParse
