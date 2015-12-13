847f0fb88f3295dbb749fc34929baf06d5ac9292
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-13 18:11:51 +0100

    add install test for perl-DateTime-Util-DayOfWeek-Locale

---
+++ tests/p_perl-DateTime-Util-DayOfWeek-Locale/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-DateTime-Util-DayOfWeek-Locale"
+t_installPackage perl-DateTime-Util-DayOfWeek-Locale
