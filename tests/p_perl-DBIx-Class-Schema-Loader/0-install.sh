a490c54a5b94e2441416472b5c44ab1cdc305e3d
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-16 19:31:17 +0100

    add install test for perl-DBIx-Class-Schema-Loader

---
+++ tests/p_perl-DBIx-Class-Schema-Loader/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-DBIx-Class-Schema-Loader"
+t_installPackage perl-DBIx-Class-Schema-Loader
