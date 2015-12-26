4e77dcf7eab392bc1d2a40eaa22555b3c9999cd9
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-26 18:24:08 +0100

    add install test for perl-Egg-Plugin-SessionKit

---
+++ tests/p_perl-Egg-Plugin-SessionKit/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Egg-Plugin-SessionKit"
+t_installPackage perl-Egg-Plugin-SessionKit
