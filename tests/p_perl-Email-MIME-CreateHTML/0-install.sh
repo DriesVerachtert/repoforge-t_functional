11570f57faf756339b3d9341cd09a1d882dc3794
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-28 18:16:37 +0100

    add install test for perl-Email-MIME-CreateHTML

---
+++ tests/p_perl-Email-MIME-CreateHTML/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Email-MIME-CreateHTML"
+t_installPackage perl-Email-MIME-CreateHTML
