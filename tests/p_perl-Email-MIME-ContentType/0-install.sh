012f89646ed5bace4e5aaa66d9b0ac6739015187
Dries Verachtert <dries.verachtert@dries.eu>
2015-12-28 18:16:37 +0100

    add install test for perl-Email-MIME-ContentType

---
+++ tests/p_perl-Email-MIME-ContentType/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Email-MIME-ContentType"
+t_installPackage perl-Email-MIME-ContentType
