89f1fc882947e64743a68c20f5f73dfe78319e3f
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-06 21:38:32 +0100

    add install test for perl-File-Find-Rule-Filesys-Virtual

---
+++ tests/p_perl-File-Find-Rule-Filesys-Virtual/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-File-Find-Rule-Filesys-Virtual"
+t_installPackage perl-File-Find-Rule-Filesys-Virtual
