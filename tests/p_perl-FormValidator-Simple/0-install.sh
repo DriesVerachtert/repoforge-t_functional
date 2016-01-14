1b2e04bb4f3ba6bde68692f0e7952beb9a9048b8
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-14 20:35:29 +0100

    add install test for perl-FormValidator-Simple

---
+++ tests/p_perl-FormValidator-Simple/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-FormValidator-Simple"
+t_installPackage perl-FormValidator-Simple
