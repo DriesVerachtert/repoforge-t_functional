e780ea9d0e8763becfe82202fb152c0b2f0d87d2
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-15 18:42:20 +0100

    add install test for perl-Games-EverQuest-LogLineParser

---
+++ tests/p_perl-Games-EverQuest-LogLineParser/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Games-EverQuest-LogLineParser"
+t_installPackage perl-Games-EverQuest-LogLineParser
