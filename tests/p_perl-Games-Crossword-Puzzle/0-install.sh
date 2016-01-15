9ba6c38d4ecf9c092e38e557e6ade6157d6e1e58
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-15 18:42:20 +0100

    add install test for perl-Games-Crossword-Puzzle

---
+++ tests/p_perl-Games-Crossword-Puzzle/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Games-Crossword-Puzzle"
+t_installPackage perl-Games-Crossword-Puzzle
