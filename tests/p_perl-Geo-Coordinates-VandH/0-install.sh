fe939c676c5659ee9f7a626bf09f76fccf0bcc27
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-18 18:14:12 +0100

    add install test for perl-Geo-Coordinates-VandH

---
+++ tests/p_perl-Geo-Coordinates-VandH/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Geo-Coordinates-VandH"
+t_installPackage perl-Geo-Coordinates-VandH
