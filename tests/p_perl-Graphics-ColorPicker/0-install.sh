71d54c6192d1f87192b417f618226127ad1d2e8a
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-25 19:02:23 +0100

    add install test for perl-Graphics-ColorPicker

---
+++ tests/p_perl-Graphics-ColorPicker/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Graphics-ColorPicker"
+t_installPackage perl-Graphics-ColorPicker
