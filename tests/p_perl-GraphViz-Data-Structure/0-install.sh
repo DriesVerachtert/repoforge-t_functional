337607797b9e7ae60c7df400e4a0721722080879
Dries Verachtert <dries.verachtert@dries.eu>
2016-01-26 18:44:22 +0100

    add install test for perl-GraphViz-Data-Structure

---
+++ tests/p_perl-GraphViz-Data-Structure/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-GraphViz-Data-Structure"
+t_installPackage perl-GraphViz-Data-Structure
