cb4317f3998ddb7d73741836af5e39f45d50e3ed
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-05 20:39:01 +0100

    add install test for perl-HTML-TokeParser-Simple

---
+++ tests/p_perl-HTML-TokeParser-Simple/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-HTML-TokeParser-Simple"
+t_installPackage perl-HTML-TokeParser-Simple
