aa1b570eccf2a5c2f104bd151a502b0873b16294
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-06 18:54:10 +0100

    add install test for perl-HTML-WikiConverter-PbWiki

---
+++ tests/p_perl-HTML-WikiConverter-PbWiki/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-HTML-WikiConverter-PbWiki"
+t_installPackage perl-HTML-WikiConverter-PbWiki
