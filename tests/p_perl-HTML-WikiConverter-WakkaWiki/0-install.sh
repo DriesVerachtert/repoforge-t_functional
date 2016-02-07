54a5da566e9d0bc79ef7f8c1e4f829ef5d444667
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-07 19:03:02 +0100

    add install test for perl-HTML-WikiConverter-WakkaWiki

---
+++ tests/p_perl-HTML-WikiConverter-WakkaWiki/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-HTML-WikiConverter-WakkaWiki"
+t_installPackage perl-HTML-WikiConverter-WakkaWiki
