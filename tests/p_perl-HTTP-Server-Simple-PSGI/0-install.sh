4c7e6f5599021bf1645f26136035ef32fd059de2
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-10 20:22:08 +0100

    add install test for perl-HTTP-Server-Simple-PSGI

---
+++ tests/p_perl-HTTP-Server-Simple-PSGI/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-HTTP-Server-Simple-PSGI"
+t_installPackage perl-HTTP-Server-Simple-PSGI
