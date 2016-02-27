281addc787aaddeafcbef35a9bceadb0ca0b84aa
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-27 21:03:49 +0100

    add install test for perl-Language-DATR-DATR2XML

---
+++ tests/p_perl-Language-DATR-DATR2XML/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Language-DATR-DATR2XML"
+t_installPackage perl-Language-DATR-DATR2XML
