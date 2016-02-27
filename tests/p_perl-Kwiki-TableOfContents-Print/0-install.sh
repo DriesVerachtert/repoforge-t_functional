2cc7a383d2bd9831d01148e6e997c27d46a359e5
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-27 21:03:49 +0100

    add install test for perl-Kwiki-TableOfContents-Print

---
+++ tests/p_perl-Kwiki-TableOfContents-Print/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Kwiki-TableOfContents-Print"
+t_installPackage perl-Kwiki-TableOfContents-Print
