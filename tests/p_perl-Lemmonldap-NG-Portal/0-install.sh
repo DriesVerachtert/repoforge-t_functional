f08dcd99e3abf4243dbd26c6bc2af72fb951c1ea
Dries Verachtert <dries.verachtert@dries.eu>
2016-02-28 20:27:16 +0100

    add install test for perl-Lemmonldap-NG-Portal

---
+++ tests/p_perl-Lemmonldap-NG-Portal/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Lemmonldap-NG-Portal"
+t_installPackage perl-Lemmonldap-NG-Portal
