bbea845f09d956aa49f425d93dde3365f05328ed
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-02 19:14:56 +0100

    add install test for perl-Lingua-DE-Wortschatz

---
+++ tests/p_perl-Lingua-DE-Wortschatz/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Lingua-DE-Wortschatz"
+t_installPackage perl-Lingua-DE-Wortschatz
