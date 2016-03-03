ae5cf4b0fa246e032d420d31b25df736df1045b9
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-03 19:48:42 +0100

    add install test for perl-Lingua-Han-Cantonese

---
+++ tests/p_perl-Lingua-Han-Cantonese/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Lingua-Han-Cantonese"
+t_installPackage perl-Lingua-Han-Cantonese
