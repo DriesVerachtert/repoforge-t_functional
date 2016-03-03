79819b26b4814be2fa84ef1f991aedc1caf8fadd
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-03 19:48:42 +0100

    add install test for perl-Lingua-EN-Numbers-Ordinate

---
+++ tests/p_perl-Lingua-EN-Numbers-Ordinate/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Lingua-EN-Numbers-Ordinate"
+t_installPackage perl-Lingua-EN-Numbers-Ordinate
