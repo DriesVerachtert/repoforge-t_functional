00b3f21543361daea9042faee6f57b84625717d0
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-04 19:36:30 +0100

    add install test for perl-Lingua-Romana-Perligata

---
+++ tests/p_perl-Lingua-Romana-Perligata/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Lingua-Romana-Perligata"
+t_installPackage perl-Lingua-Romana-Perligata
