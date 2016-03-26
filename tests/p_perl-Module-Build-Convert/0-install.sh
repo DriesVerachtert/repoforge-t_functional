6d709162537acb6e43c60cd378f54dc49786de3a
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-26 19:45:21 +0100

    add install test for perl-Module-Build-Convert

---
+++ tests/p_perl-Module-Build-Convert/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Module-Build-Convert"
+t_installPackage perl-Module-Build-Convert
