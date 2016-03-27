5b27e4e163b8581629bfd130f3136239a09624e9
Dries Verachtert <dries.verachtert@dries.eu>
2016-03-27 20:14:19 +0200

    add install test for perl-Module-Load-Conditional

---
+++ tests/p_perl-Module-Load-Conditional/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Module-Load-Conditional"
+t_installPackage perl-Module-Load-Conditional
