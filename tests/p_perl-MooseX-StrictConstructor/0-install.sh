2c62d75ccf97086f71b54215c02d55dbbd284f02
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-01 21:47:47 +0200

    add install test for perl-MooseX-StrictConstructor

---
+++ tests/p_perl-MooseX-StrictConstructor/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-MooseX-StrictConstructor"
+t_installPackage perl-MooseX-StrictConstructor
