1bddf72b2fd5d3fb477b1973a6486d46ec1408d6
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-02 19:50:45 +0200

    add install test for perl-MouseX-AttributeHelpers

---
+++ tests/p_perl-MouseX-AttributeHelpers/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-MouseX-AttributeHelpers"
+t_installPackage perl-MouseX-AttributeHelpers
