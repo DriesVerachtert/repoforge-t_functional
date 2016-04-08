4040f30635692f57d59943581a3ca0d9cc3e96b0
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-08 19:28:42 +0200

    add install test for perl-Net-Domain-ExpireDate

---
+++ tests/p_perl-Net-Domain-ExpireDate/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Net-Domain-ExpireDate"
+t_installPackage perl-Net-Domain-ExpireDate
