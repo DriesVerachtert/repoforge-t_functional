8e40b9acb0c2b17277cfce8732ee9e38722f172c
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-09 21:28:39 +0200

    add install test for perl-Net-FTP-AutoReconnect

---
+++ tests/p_perl-Net-FTP-AutoReconnect/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Net-FTP-AutoReconnect"
+t_installPackage perl-Net-FTP-AutoReconnect
