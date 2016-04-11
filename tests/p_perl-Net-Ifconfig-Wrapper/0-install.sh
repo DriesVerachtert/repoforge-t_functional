e6147d2d26c71fb6c0b5f61f4e0a69a4fe4a7e17
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-11 19:38:06 +0200

    add install test for perl-Net-Ifconfig-Wrapper

---
+++ tests/p_perl-Net-Ifconfig-Wrapper/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Net-Ifconfig-Wrapper"
+t_installPackage perl-Net-Ifconfig-Wrapper
