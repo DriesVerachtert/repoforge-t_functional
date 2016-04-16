4527119a3562d5463536177ca01a439ddc165635
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-16 19:37:57 +0200

    add install test for perl-Net-Rendezvous-Publish

---
+++ tests/p_perl-Net-Rendezvous-Publish/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Net-Rendezvous-Publish"
+t_installPackage perl-Net-Rendezvous-Publish
