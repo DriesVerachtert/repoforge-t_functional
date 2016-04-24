38bfa635ac7683f4f34117028336ad950d090941
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-24 19:38:13 +0200

    add install test for perl-Object-Realize-Later

---
+++ tests/p_perl-Object-Realize-Later/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Object-Realize-Later"
+t_installPackage perl-Object-Realize-Later
