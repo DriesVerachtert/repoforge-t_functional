fc658b7d22378c41387bf36bc40d0d0848181a7c
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-26 20:04:53 +0200

    add install test for perl-Package-DeprecationManager

---
+++ tests/p_perl-Package-DeprecationManager/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Package-DeprecationManager"
+t_installPackage perl-Package-DeprecationManager
