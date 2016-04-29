1aadfbeee29f808b85740ab131db47195648c811
Dries Verachtert <dries.verachtert@dries.eu>
2016-04-29 21:38:22 +0200

    add install test for perl-Path-Class-File-Stat

---
+++ tests/p_perl-Path-Class-File-Stat/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Path-Class-File-Stat"
+t_installPackage perl-Path-Class-File-Stat
