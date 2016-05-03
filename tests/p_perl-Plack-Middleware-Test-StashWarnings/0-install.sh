a91bc431aa7ea0b11178acf41fd9bb4cfb71cea8
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-03 21:27:15 +0200

    add install test for perl-Plack-Middleware-Test-StashWarnings

---
+++ tests/p_perl-Plack-Middleware-Test-StashWarnings/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Plack-Middleware-Test-StashWarnings"
+t_installPackage perl-Plack-Middleware-Test-StashWarnings
