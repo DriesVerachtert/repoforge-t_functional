1b7198c3abec17ace87d9c109a10b1680a128a1d
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-06 20:03:36 +0200

    add install test for perl-POE-Component-Client-NNTP

---
+++ tests/p_perl-POE-Component-Client-NNTP/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-POE-Component-Client-NNTP"
+t_installPackage perl-POE-Component-Client-NNTP
