36c96b8fbf5670688105e05b3f10ea3d466be9b3
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-07 19:32:35 +0200

    add install test for perl-POE-Component-CPAN-YACSmoke

---
+++ tests/p_perl-POE-Component-CPAN-YACSmoke/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-POE-Component-CPAN-YACSmoke"
+t_installPackage perl-POE-Component-CPAN-YACSmoke
