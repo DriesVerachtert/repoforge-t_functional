fc18c7f0f3000fbdecfbff10157a536794098e7d
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-10 20:41:36 +0200

    add install test for perl-POE-Component-SimpleLog

---
+++ tests/p_perl-POE-Component-SimpleLog/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-POE-Component-SimpleLog"
+t_installPackage perl-POE-Component-SimpleLog
