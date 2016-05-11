07c28b2ee3c52df1877aacbe910ce64d18a077a1
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-11 22:45:52 +0200

    add install test for perl-POE-Filter-Log-IPTables

---
+++ tests/p_perl-POE-Filter-Log-IPTables/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-POE-Filter-Log-IPTables"
+t_installPackage perl-POE-Filter-Log-IPTables
