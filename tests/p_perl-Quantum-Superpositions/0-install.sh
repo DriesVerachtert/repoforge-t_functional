41e064d375b3d8d27aabd832b683d683052aaad6
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-16 20:55:13 +0200

    add install test for perl-Quantum-Superpositions

---
+++ tests/p_perl-Quantum-Superpositions/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Quantum-Superpositions"
+t_installPackage perl-Quantum-Superpositions
