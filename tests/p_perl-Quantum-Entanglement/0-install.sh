ec5f53a58c435470cceb3edeaad718946e375912
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-16 20:55:13 +0200

    add install test for perl-Quantum-Entanglement

---
+++ tests/p_perl-Quantum-Entanglement/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Quantum-Entanglement"
+t_installPackage perl-Quantum-Entanglement
