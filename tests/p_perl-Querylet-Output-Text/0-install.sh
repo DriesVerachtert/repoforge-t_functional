fd920e3b7ffe49172d6c994c382ebe34df7b9d3c
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-17 20:17:06 +0200

    add install test for perl-Querylet-Output-Text

---
+++ tests/p_perl-Querylet-Output-Text/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Querylet-Output-Text"
+t_installPackage perl-Querylet-Output-Text
