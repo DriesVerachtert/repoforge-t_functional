8f8535766f6ae4fd7bed71ed0f5f2343aff7e2a3
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-17 20:17:06 +0200

    add install test for perl-Querylet-Output-Excel-OLE

---
+++ tests/p_perl-Querylet-Output-Excel-OLE/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Querylet-Output-Excel-OLE"
+t_installPackage perl-Querylet-Output-Excel-OLE
