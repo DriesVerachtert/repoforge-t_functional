8817dfc0a91e19f3a97687730c7568cf057e7e3e
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-22 21:50:39 +0200

    add install test for perl-RT-Extension-MobileUI

---
+++ tests/p_perl-RT-Extension-MobileUI/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-RT-Extension-MobileUI"
+t_installPackage perl-RT-Extension-MobileUI
