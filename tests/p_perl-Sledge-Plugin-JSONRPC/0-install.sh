a6071a37b234e8c6059dbe9664073d759a1df46d
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-26 21:14:25 +0200

    add install test for perl-Sledge-Plugin-JSONRPC

---
+++ tests/p_perl-Sledge-Plugin-JSONRPC/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Sledge-Plugin-JSONRPC"
+t_installPackage perl-Sledge-Plugin-JSONRPC
