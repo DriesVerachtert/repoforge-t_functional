f91304c37ee7cafb25794d96db0f5a5091a8c08c
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-29 22:59:38 +0200

    add install test for perl-Spreadsheet-ParseExcel

---
+++ tests/p_perl-Spreadsheet-ParseExcel/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Spreadsheet-ParseExcel"
+t_installPackage perl-Spreadsheet-ParseExcel
