5a86dc0e1508427539b0f3e819c8b3ec7f9b4333
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-29 22:59:38 +0200

    add install test for perl-Spreadsheet-SimpleExcel

---
+++ tests/p_perl-Spreadsheet-SimpleExcel/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Spreadsheet-SimpleExcel"
+t_installPackage perl-Spreadsheet-SimpleExcel
