4ba7007d2f11b0e47368b198baead4ddc6f8e2ef
Dries Verachtert <dries.verachtert@dries.eu>
2016-05-31 22:08:51 +0200

    add install test for perl-Statistics-Descriptive

---
+++ tests/p_perl-Statistics-Descriptive/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Statistics-Descriptive"
+t_installPackage perl-Statistics-Descriptive
