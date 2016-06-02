2366405706731668aa9cbc9326c18d8ab95b68d0
Dries Verachtert <dries.verachtert@dries.eu>
2016-06-02 19:20:53 +0200

    add install test for perl-Statistics-Smoothing-SGT

---
+++ tests/p_perl-Statistics-Smoothing-SGT/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Statistics-Smoothing-SGT"
+t_installPackage perl-Statistics-Smoothing-SGT
