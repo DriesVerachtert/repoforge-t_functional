59fbf71c51ed0e76970888672c4601b695b0834a
Dries Verachtert <dries.verachtert@dries.eu>
2016-06-13 21:49:21 +0200

    add install test for perl-Template-Plugin-Number-Format

---
+++ tests/p_perl-Template-Plugin-Number-Format/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Template-Plugin-Number-Format"
+t_installPackage perl-Template-Plugin-Number-Format
