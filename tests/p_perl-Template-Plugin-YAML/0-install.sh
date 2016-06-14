777e5c50fd7338a11433eed1eb3c16ef0ee0d900
Dries Verachtert <dries.verachtert@dries.eu>
2016-06-14 19:48:16 +0200

    add install test for perl-Template-Plugin-YAML

---
+++ tests/p_perl-Template-Plugin-YAML/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Template-Plugin-YAML"
+t_installPackage perl-Template-Plugin-YAML
