2029e634b7fcb1b41c2511e15083e11e60880272
Dries Verachtert <dries.verachtert@dries.eu>
2016-06-30 22:13:27 +0200

    add install test for perl-Text-Markdown-Discount

---
+++ tests/p_perl-Text-Markdown-Discount/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Text-Markdown-Discount"
+t_installPackage perl-Text-Markdown-Discount
