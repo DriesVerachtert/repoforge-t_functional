256840a3ba1c769f49ecd06c002610a3a54c792a
Dries Verachtert <dries.verachtert@dries.eu>
2016-07-05 20:48:02 +0200

    add install test for perl-Thread-Conveyor-Monitored

---
+++ tests/p_perl-Thread-Conveyor-Monitored/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Thread-Conveyor-Monitored"
+t_installPackage perl-Thread-Conveyor-Monitored
