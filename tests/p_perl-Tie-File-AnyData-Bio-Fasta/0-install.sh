1a0db534d4b2373731e5fcdafb0558b2626aaaee
Dries Verachtert <dries.verachtert@dries.eu>
2016-07-08 19:59:21 +0200

    add install test for perl-Tie-File-AnyData-Bio-Fasta

---
+++ tests/p_perl-Tie-File-AnyData-Bio-Fasta/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Tie-File-AnyData-Bio-Fasta"
+t_installPackage perl-Tie-File-AnyData-Bio-Fasta
