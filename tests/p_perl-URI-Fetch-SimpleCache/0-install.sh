0182550567a5f1865d9521a57719f0832fe50db6
Dries Verachtert <dries.verachtert@dries.eu>
2016-07-23 21:20:21 +0200

    add install test for perl-URI-Fetch-SimpleCache

---
+++ tests/p_perl-URI-Fetch-SimpleCache/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-URI-Fetch-SimpleCache"
+t_installPackage perl-URI-Fetch-SimpleCache
