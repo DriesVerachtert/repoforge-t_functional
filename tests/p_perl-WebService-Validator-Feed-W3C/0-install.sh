85360972ea56bbfef0d426df2a1898c3709b5064
Dries Verachtert <dries.verachtert@dries.eu>
2016-07-31 22:40:50 +0200

    add install test for perl-WebService-Validator-Feed-W3C

---
+++ tests/p_perl-WebService-Validator-Feed-W3C/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-WebService-Validator-Feed-W3C"
+t_installPackage perl-WebService-Validator-Feed-W3C
