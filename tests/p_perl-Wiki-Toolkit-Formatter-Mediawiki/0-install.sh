6821a387f2ab57ddf73388fc0261c061a424bd06
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-01 22:26:41 +0200

    add install test for perl-Wiki-Toolkit-Formatter-Mediawiki

---
+++ tests/p_perl-Wiki-Toolkit-Formatter-Mediawiki/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Wiki-Toolkit-Formatter-Mediawiki"
+t_installPackage perl-Wiki-Toolkit-Formatter-Mediawiki
