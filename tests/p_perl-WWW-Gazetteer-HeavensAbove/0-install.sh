5764409b1049c97006e88960339a727b1ac37b0e
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-02 21:37:13 +0200

    add install test for perl-WWW-Gazetteer-HeavensAbove

---
+++ tests/p_perl-WWW-Gazetteer-HeavensAbove/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-WWW-Gazetteer-HeavensAbove"
+t_installPackage perl-WWW-Gazetteer-HeavensAbove
