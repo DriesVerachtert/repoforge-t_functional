81e96442b7289b9be1525183c7596424beba14cd
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-03 20:25:56 +0200

    add install test for perl-WWW-Mechanize-Cached

---
+++ tests/p_perl-WWW-Mechanize-Cached/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-WWW-Mechanize-Cached"
+t_installPackage perl-WWW-Mechanize-Cached
