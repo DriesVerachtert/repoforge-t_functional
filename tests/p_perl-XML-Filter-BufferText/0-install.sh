e1ec6c850d7a03396a082d7b2c0c0a76bdc1acf2
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-09 21:20:10 +0200

    add install test for perl-XML-Filter-BufferText

---
+++ tests/p_perl-XML-Filter-BufferText/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-XML-Filter-BufferText"
+t_installPackage perl-XML-Filter-BufferText
