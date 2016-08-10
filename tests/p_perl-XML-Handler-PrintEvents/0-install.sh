b717bc6aed5a2ce682eabcc1e291826b36d2410b
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-10 20:38:48 +0200

    add install test for perl-XML-Handler-PrintEvents

---
+++ tests/p_perl-XML-Handler-PrintEvents/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-XML-Handler-PrintEvents"
+t_installPackage perl-XML-Handler-PrintEvents
