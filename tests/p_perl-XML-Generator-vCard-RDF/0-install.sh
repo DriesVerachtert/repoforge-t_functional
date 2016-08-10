e47a247534d82fbd3609cc0e8ee5ef50e5fbd7e8
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-10 20:38:48 +0200

    add install test for perl-XML-Generator-vCard-RDF

---
+++ tests/p_perl-XML-Generator-vCard-RDF/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-XML-Generator-vCard-RDF"
+t_installPackage perl-XML-Generator-vCard-RDF
