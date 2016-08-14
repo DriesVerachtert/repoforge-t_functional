9ce0908b8e6a3207a935b0fc5232d57d50c3be8a
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-14 19:38:59 +0200

    add install test for perl-XML-RSS-TimingBotDBI

---
+++ tests/p_perl-XML-RSS-TimingBotDBI/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-XML-RSS-TimingBotDBI"
+t_installPackage perl-XML-RSS-TimingBotDBI
