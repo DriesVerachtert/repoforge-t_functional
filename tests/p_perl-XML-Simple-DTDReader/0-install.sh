bcbe679d05d2fd0eb6a527af497245cea0e4cf4b
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-15 21:20:57 +0200

    add install test for perl-XML-Simple-DTDReader

---
+++ tests/p_perl-XML-Simple-DTDReader/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-XML-Simple-DTDReader"
+t_installPackage perl-XML-Simple-DTDReader
