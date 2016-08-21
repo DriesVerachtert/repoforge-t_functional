34d0326ebefde7b76539e049947c3df6d73fe7f0
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-21 20:58:57 +0200

    add install test for perl-Youri-Package-RPM-Generator

---
+++ tests/p_perl-Youri-Package-RPM-Generator/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install perl-Youri-Package-RPM-Generator"
+t_installPackage perl-Youri-Package-RPM-Generator
