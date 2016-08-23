2ee291feeff358cbac9d55be7bf5b4c30862fd52
Dries Verachtert <dries.verachtert@dries.eu>
2016-08-23 19:59:29 +0200

    typo fix

--- tests/p_perl-URI-Title/0-install.sh
+++ tests/p_perl-URI-Title/0-install.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Author: Dries Verachtert <dries.verachert@dries.eu>
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
 
 t_Log "Running $0 - attempting to install perl-URI-Title"
-t_InstallPackage perl-URI-Title
+t_installPackage perl-URI-Title
