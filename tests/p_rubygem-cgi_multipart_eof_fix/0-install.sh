e3f64395356e14067c74f5fbe7f27cb1050f7362
Dries Verachtert <dries.verachtert@dries.eu>
2016-10-06 21:24:45 +0200

    add install test for rubygem-cgi_multipart_eof_fix

---
+++ tests/p_rubygem-cgi_multipart_eof_fix/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install rubygem-cgi_multipart_eof_fix"
+t_installPackage rubygem-cgi_multipart_eof_fix
