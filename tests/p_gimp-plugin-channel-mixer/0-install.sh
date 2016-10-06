54b90ee0ab4e402dc1e883e5ec07de4fd9832135
Dries Verachtert <dries.verachtert@dries.eu>
2016-10-06 21:22:37 +0200

    add install test for gimp-plugin-channel-mixer

---
+++ tests/p_gimp-plugin-channel-mixer/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install gimp-plugin-channel-mixer"
+t_installPackage gimp-plugin-channel-mixer
