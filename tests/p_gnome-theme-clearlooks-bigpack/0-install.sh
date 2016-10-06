c1b937b1ff1707d5cd75253002fec13636655c1b
Dries Verachtert <dries.verachtert@dries.eu>
2016-10-06 21:22:41 +0200

    add install test for gnome-theme-clearlooks-bigpack

---
+++ tests/p_gnome-theme-clearlooks-bigpack/0-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Dries Verachtert <dries.verachtert@dries.eu>
+
+t_Log "Running $0 - attempting to install gnome-theme-clearlooks-bigpack"
+t_installPackage gnome-theme-clearlooks-bigpack
